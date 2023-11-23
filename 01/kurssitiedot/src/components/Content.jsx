330853d466bbcafb90c9855a1038f754a04a50f1
SaaraL <saara@leppihalme.fi>
2023-11-23 20:25:01 +0200

    step 1: App with components

---
+++ 01/kurssitiedot/src/components/Content.jsx
@@ -0,0 +1,16 @@
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part[0]} {props.exercise[0]}
+      </p>
+      <p>
+        {props.part[1]} {props.exercise[1]}
+      </p>
+      <p>
+        {props.part[2]} {props.exercise[2]}
+      </p>
+    </div>
+  );
+};
+export default Content;
