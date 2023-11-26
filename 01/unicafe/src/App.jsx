1f611f0fb5e97ffa0069b1bf41fef4c6c6213528
SaaraL <saara@leppihalme.fi>
2023-11-26 18:35:39 +0200

    1.7 added more statistics and counter state to count total clicks

--- 01/unicafe/src/App.jsx
+++ 01/unicafe/src/App.jsx
@@ -4,19 +4,23 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   const handleGood = () => {
     setGood(good + 1);
+    setCounter(counter + 1);
   };
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setCounter(counter + 1);
   };
   const handleBad = () => {
     setBad(bad + 1);
+    setCounter(counter + 1);
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>give feedback</h1>
       <div className="button-section">
         <button id="good" onClick={handleGood}>
@@ -35,6 +39,14 @@ const App = () => {
           <p>Good: {good}</p>
           <p>Neutral: {neutral}</p>
           <p>Bad: {bad}</p>
+          <p>All {good + bad + neutral}</p>
+          <p>
+            Average{" "}
+            {counter === 0
+              ? "-"
+              : (good * 1 + bad * -1 + neutral * 0) / counter}
+          </p>
+          <p>Positive {counter === 0 ? "-" : (good / counter) * 100} %</p>
         </div>
       </div>
     </div>
