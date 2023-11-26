1ebf5e8f1e7b641631467ac2367a31b90779c219
SaaraL <saara@leppihalme.fi>
2023-11-26 18:13:38 +0200

    1.6 functionalities working

--- 01/unicafe/src/App.jsx
+++ 01/unicafe/src/App.jsx
@@ -1,9 +1,44 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
   return (
     <div>
-      <p>Hello World!</p>
+      <h1>give feedback</h1>
+      <div className="button-section">
+        <button id="good" onClick={handleGood}>
+          good
+        </button>
+        <button id="neutral" onClick={handleNeutral}>
+          neutral
+        </button>
+        <button id="bad" onClick={handleBad}>
+          bad
+        </button>
+      </div>
+      <div>
+        <h2>Statistics</h2>
+        <div className="statistic-section">
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
