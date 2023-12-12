03e4f1f676c492ed699dee59850aff6ec2ae4379
SaaraL <saara@leppihalme.fi>
2023-12-12 18:08:59 +0200

    2.6 phonebook step1, basic phonebook which stores and lists names, CSS styling

--- 02/phonebook/src/App.jsx
+++ 02/phonebook/src/App.jsx
@@ -1,8 +1,41 @@
-function App() {
+import { useState } from "react"
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+
+  const [newName, setNewName] = useState("")
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName("")
+  }
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div>
+      <div className="form-section">
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input value={newName} onChange={handlePersonChange} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+      <div className="list-section">
+        <h2>Numbers</h2>
+        <ul className="persons">
+          {persons.map((person, i) => (
+            <li key={i}>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
