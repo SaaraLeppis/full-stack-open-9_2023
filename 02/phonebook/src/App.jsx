900b6375632404ac14f2905656643033ef14b7b5
SaaraL <saara@leppihalme.fi>
2023-12-13 11:31:02 +0200

    *2.9 step 4: search field for names

--- 02/phonebook/src/App.jsx
+++ 02/phonebook/src/App.jsx
@@ -3,10 +3,14 @@ import { useState } from "react"
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123 321" },
+    { name: "Sami Metsälä", number: "040-555 555" },
+    { name: "Tiina Taneli", number: "040-621 321" },
+    { name: "Aamu Aikainen", number: "044-999 321" },
   ])
 
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [searchValue, setSearchValue] = useState("")
 
   //function to check if name exists
   const nameExists = (checkName) => {
@@ -26,11 +30,19 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value)
+  }
 
   return (
     <div>
       <div className="form-section">
         <h2>Phonebook</h2>
+        <div id="input-section">
+          <label htmlFor="nameFilter">filter shown with:</label>
+          <input id="nameFilter" value={searchValue} onChange={handleSearch} />
+        </div>
+        <h2>add a new</h2>
         <form onSubmit={addPerson}>
           <div id="input-section">
             <label htmlFor="newName">name:</label>
@@ -50,11 +62,23 @@ const App = () => {
       <div className="list-section">
         <h2>Numbers</h2>
         <ul className="persons">
-          {persons.map((person, i) => (
-            <li key={i}>
-              {person.name} {person.number}
-            </li>
-          ))}
+          {/*           {showAll &&
+            persons.map((person, i) => (
+              <li key={i}>
+                {person.name} {person.number}
+              </li>
+            ))} */}
+          {persons
+            .filter(
+              (person) =>
+                person.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+                searchValue === ""
+            )
+            .map((person) => (
+              <li key={person.name}>
+                {person.name} {person.number}
+              </li>
+            ))}
         </ul>
       </div>
     </div>
