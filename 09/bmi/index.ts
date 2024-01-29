57afcde1c95784e32cfb27a64c446b8938b973ef
SaaraL <saara@leppihalme.fi>
2024-01-29 20:06:51 +0200

    9.5 WebBMI: parameters from query, calculator module in 'utils' folder

--- 09/bmi/index.ts
+++ 09/bmi/index.ts
@@ -1,8 +1,22 @@
-import express from 'express'
-const app = express()
+import express, { Application, Request, Response } from 'express'
+import calculateWebBmi from './utils/webBmi'
+
+const app: Application = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack')
+})
+
+app.get('/bmi', (req: Request, res: Response) => {
+  const { height, weight } = req.query
+  const validParameters: boolean =
+    !isNaN(Number(height)) && !isNaN(Number(weight))
+
+  const bmi = calculateWebBmi(Number(height), Number(weight))
+  if (!validParameters || !height || !weight) {
+    res.status(400).send({ error: 'malformatted parameters' })
+  }
+  res.send(bmi)
 })
 
 const PORT = 3003
