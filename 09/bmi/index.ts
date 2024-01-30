c2ccf7604c21a3cf639f64f67becc6b19e598935
SaaraL <saara@leppihalme.fi>
2024-01-30 15:48:59 +0200

    9.6 Eslint  adding eslint configurations to project

--- 09/bmi/index.ts
+++ 09/bmi/index.ts
@@ -1,26 +1,26 @@
-import express, { Application, Request, Response } from 'express'
-import calculateWebBmi from './utils/webBmi'
+import express from 'express';
+import calculateWebBmi from './utils/webBmi';
 
-const app: Application = express()
+const app = express();
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack')
-})
+  res.send('Hello Full Stack');
+});
 
-app.get('/bmi', (req: Request, res: Response) => {
-  const { height, weight } = req.query
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
   const validParameters: boolean =
-    !isNaN(Number(height)) && !isNaN(Number(weight))
+    !isNaN(Number(height)) && !isNaN(Number(weight));
 
-  const bmi = calculateWebBmi(Number(height), Number(weight))
+  const bmi = calculateWebBmi(Number(height), Number(weight));
   if (!validParameters || !height || !weight) {
-    res.status(400).send({ error: 'malformatted parameters' })
+    res.status(400).send({ error: 'malformatted parameters' });
   }
-  res.send(bmi)
-})
+  res.send(bmi);
+});
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
