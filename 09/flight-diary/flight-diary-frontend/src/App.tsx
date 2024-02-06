44850127b51439bf1a149ab445bb78c659b910e1
SaaraL <saara@leppihalme.fi>
2024-02-06 20:01:22 +0200

    9.16 diary entries rendered

--- 09/flight-diary/flight-diary-frontend/src/App.tsx
+++ 09/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,34 +1,71 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-interface Entries {
-  id: number;
-  date: string;
-  weather: string;
-  visibility: string;
-}
+import { Entry } from './types';
 
 function App() {
-  const [diaryData, setDiaryData] = useState<Entries[]>([]);
-  useEffect(() => {
-    axios.get<Entries[]>('http://localhost:3000/api/diaries').then(response => {
+  const [entries, setEntries] = useState<Entry[]>([]);
+  /*   const [newEntry, setNewEntry] = useState<Entry>(id:'', date:'', visibility:'', weather:'', comment:'' );
+   */ useEffect(() => {
+    axios.get<Entry[]>('http://localhost:3000/api/diaries').then(response => {
       const dataInDiary = response.data;
       console.log(dataInDiary);
-      setDiaryData(dataInDiary as Entries[]);
+      setEntries(dataInDiary as Entry[]);
     });
   }, []);
 
+  /*   const entryCreation = (event) => {
+    const {name,value} =event.target;
+    setNewEntry ({...newEntry, [name]:value})
+  }; */
+
   return (
-    <>
+    <div className="main-container">
       <h1>Flight diary</h1>
-      <div className="entires">
+      {/* <div className="form-container">
+        <h2>Add new entry</h2>
+        <form onSubmit={entryCreation}>
+          <label htmlFor="date">date: </label>
+          <input
+            type="text"
+            name="date"
+            value={newEntry}
+            onChange={event => setNewEntry(event.target.value)}
+          />
+          <label htmlFor="visibility">visibility: </label>
+          <input
+            type="text"
+            name="visibility"
+            value={newEntry}
+            onChange={event => setNewEntry(event.target.value)}
+          />
+          <label htmlFor="weather">Weather: </label>
+          <input
+            type="text"
+            name="weather"
+            value={newEntry}
+            onChange={event => setNewEntry(event.target.value)}
+          />
+          <label htmlFor="comment">Comment: </label>
+          <input
+            type="text"
+            name="comment"
+            value={newEntry}
+            onChange={event => setNewEntry(event.target.value)}
+          />
+          <button type="submit">add +</button>
+        </form>
+      </div> */}
+      <div className="entires-container">
         <h2>Diary entries</h2>
-        {diaryData.map(data => (
-          <li key={data.id}>
-            {data.date} {data.weather} {data.visibility}
-          </li>
+        {entries.map(data => (
+          <div key={data.id} className="single-entry">
+            <h3>{data.date}</h3>
+            <p>visibility: {data.visibility}</p>
+            <p>weather: {data.weather}</p>
+          </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
