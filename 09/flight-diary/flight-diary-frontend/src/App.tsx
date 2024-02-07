57f7da40795f1d738a98aa043c812df9ca38cd52
SaaraL <saara@leppihalme.fi>
2024-02-07 15:53:52 +0200

    9.17 adding new entries from frontend, better types

--- 09/flight-diary/flight-diary-frontend/src/App.tsx
+++ 09/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,60 +1,101 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Entry } from './types';
+import { Entry, Visibility, Weather } from './types';
 
 function App() {
   const [entries, setEntries] = useState<Entry[]>([]);
-  /*   const [newEntry, setNewEntry] = useState<Entry>(id:'', date:'', visibility:'', weather:'', comment:'' );
-   */ useEffect(() => {
+  const [newEntry, setNewEntry] = useState<Entry>({
+    date: '',
+    visibility: '' as Visibility,
+    weather: '' as Weather,
+    comment: '',
+  });
+  const fetchData = () => {
     axios.get<Entry[]>('http://localhost:3000/api/diaries').then(response => {
       const dataInDiary = response.data;
       console.log(dataInDiary);
       setEntries(dataInDiary as Entry[]);
     });
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
+  /* useEffect(() => {
+    axios.get<Entry[]>('http://localhost:3000/api/diaries').then(response => {
+      const dataInDiary = response.data;
+      console.log(dataInDiary);
+      setEntries(dataInDiary as Entry[]);
+    });
+  }, []); */
 
-  /*   const entryCreation = (event) => {
-    const {name,value} =event.target;
-    setNewEntry ({...newEntry, [name]:value})
-  }; */
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setNewEntry({ ...newEntry, [name]: value });
+  };
 
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      axios
+        .post('http://localhost:3000/api/diaries', newEntry)
+        .then(response => response.data)
+        .then(data => {
+          setEntries(entries.concat(data));
+        });
+
+      setNewEntry({
+        date: '',
+        visibility: '' as Visibility,
+        weather: '' as Weather,
+        comment: '',
+      });
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="main-container">
       <h1>Flight diary</h1>
-      {/* <div className="form-container">
+      <div className="form-container">
         <h2>Add new entry</h2>
-        <form onSubmit={entryCreation}>
+        <form onSubmit={submitHandler}>
           <label htmlFor="date">date: </label>
           <input
             type="text"
+            id="date"
             name="date"
-            value={newEntry}
-            onChange={event => setNewEntry(event.target.value)}
+            value={newEntry.date}
+            onChange={changeHandler}
           />
           <label htmlFor="visibility">visibility: </label>
           <input
             type="text"
+            id="visibility"
             name="visibility"
-            value={newEntry}
-            onChange={event => setNewEntry(event.target.value)}
+            value={newEntry.visibility}
+            onChange={changeHandler}
           />
           <label htmlFor="weather">Weather: </label>
           <input
             type="text"
+            id="weather"
             name="weather"
-            value={newEntry}
-            onChange={event => setNewEntry(event.target.value)}
+            value={newEntry.weather}
+            onChange={changeHandler}
           />
           <label htmlFor="comment">Comment: </label>
           <input
             type="text"
+            id="comment"
             name="comment"
-            value={newEntry}
-            onChange={event => setNewEntry(event.target.value)}
+            value={newEntry.comment}
+            onChange={changeHandler}
           />
           <button type="submit">add +</button>
         </form>
-      </div> */}
+      </div>
       <div className="entires-container">
         <h2>Diary entries</h2>
         {entries.map(data => (
--- 09/flight-diary/flight-diary-frontend/src/types.ts
+++ 09/flight-diary/flight-diary-frontend/src/types.ts
@@ -1,6 +1,26 @@
-export interface Entry {
-  id: number;
-  date: string;
-  weather: string;
-  visibility: string;
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
 }
+
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
+
+export interface Entry {
+  id?: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
+
+export type NewDiaryEntry = Omit<Entry, 'id'>;
+
+export type NonSensitiveDiaryEntry = Omit<Entry, 'comment'>;
