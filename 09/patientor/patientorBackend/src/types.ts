68aa19978220e529d214695b58e716d42e066756
SaaraL <saara@leppihalme.fi>
2024-02-10 15:50:29 +0200

    9.22 Entry types defined

--- 09/patientor/patientorBackend/src/types.ts
+++ 09/patientor/patientorBackend/src/types.ts
@@ -8,7 +8,7 @@ export enum Gender {
   Female = 'female',
   Other = 'other',
 }
-export interface Entry {}
+//export interface Entry {}
 export interface Patient {
   id: string;
   name: string;
@@ -22,3 +22,43 @@ export interface Patient {
 export type NonSensitivePatientInfo = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
+
+interface BaseEntry {
+  id: string;
+  date: string;
+  description: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnoses['code']>;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry
+  | HospitalEntry;
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
